ad1118b0e218e9234bcbd5dae43b54c6468f68ed
perlatex <wangmj@sicnu.edu.cn>
2022-07-02 13:26:22 +0800

    whyR

---
+++ 00_whyR/build.R
@@ -0,0 +1,3 @@
+# remotes::install_github("jhelvy/renderthis", dependencies = TRUE)
+renderthis::to_pdf("whyR.html", partial_slides = TRUE)
+
---
+++ 03_Rmarkdown/demo_Rmd/build.R
@@ -0,0 +1,3 @@
+# remotes::install_github("jhelvy/renderthis", dependencies = TRUE)
+renderthis::to_pdf("06_demo_xaringan.html", partial_slides = TRUE)
+
